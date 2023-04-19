d7fc80a69244d5cf70decebc539a37c1d2835bae
RubTheKid <you@example.com>
2023-04-19 10:30:12 -0300

    correções

--- exerciciosPoo1/SelecionarExercicio.cs
+++ exerciciosPoo1/SelecionarExercicio.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace exerciciosPoo1;
+﻿namespace exerciciosPoo1;
 
 public class SelecionarExercicio
 {
